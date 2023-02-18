175d40c419facd1f652e9b83f02ec2b269d5e1a1
eschaefer00 <elisa@elmama.de>
2023-02-18 17:43:09 +0100

    pipenv and db creation

--- website/auth.py
+++ website/auth.py
@@ -1,18 +1,39 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email='email').first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again', category='error')
+        else:
+            flash('There was no user found with this email', category='error')
+
     data = request.form
     print(data)
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
@@ -23,7 +44,10 @@ def sign_up():
         pw1 = request.form.get('pw1')
         pw2 = request.form.get('pw2')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email='email').first()
+        if user:
+            flash('This email has already been used', category='error')
+        elif len(email) < 4:
             flash('Email must be greater than 4 characters.', category='error')
         elif len(firstName) < 2:
             flash('First name must be greater than 3 characters.', category='error')
@@ -33,6 +57,11 @@ def sign_up():
             flash('Password must be at least 7 characters.', category='error')
         else:
             # add user to database
+            user = User(email=email, first_name=firstName, password=generate_password_hash(pw1, method='sha256'))
+            db.session.add(user)
+            db.session.commit()
+            login_user(user, remember=True)
             flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
 
-    return render_template("sign-up.html")
+    return render_template("sign-up.html", user=current_user)
